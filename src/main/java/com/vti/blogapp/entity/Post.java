35367a504316affb88695fcc4f7ba0d43e31ff74
Quyen Lh <lehuuquyen1995@gmail.com>
2023-12-02 20:36:01 +0700

    add project blogapp upgrage optimal version Spring

---
+++ src/main/java/com/vti/blogapp/entity/Post.java
@@ -0,0 +1,36 @@
+package com.vti.blogapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 100)
+    private String description;
+
+    @Column(name = "content", nullable = false, length = 100)
+    private String content;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+}
